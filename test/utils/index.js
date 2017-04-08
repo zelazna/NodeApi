694851d58fc9a187f93b5c9932679dac8d49406f
zelazna <constantin.guidon@gmail.com>
2017-04-08 21:26:05 +0200

    [feature] .eslintignore

---
+++ test/utils/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+
+const currentDirectory = path.dirname(__filename)
+const resourcesDirectory = path.join(currentDirectory, '..', 'resources')
+
+function resourcePath (filePath) {
+  return path.join(resourcesDirectory, filePath)
+}
+
+function loadJsonResource (filePath) {
+  const file = resourcePath(filePath)
+  const data = fs.readFileSync(file, 'utf8')
+  return JSON.parse(data)
+}
+
+module.exports = loadJsonResource
