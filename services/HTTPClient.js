d11b70044abd30079f1c00fa71e9b7117178f970
zelazna <constantin.guidon@gmail.com>
2017-04-11 22:03:44 +0200

    [httpClient] changes and test

--- services/HTTPClient.js
+++ services/HTTPClient.js
@@ -1,49 +1,28 @@
-const http = require('https')
-
 class HTTPCLient {
-  constructor (baseUrl) {
+  constructor (baseUrl, http) {
     this.baseUrl = baseUrl
+    this.http = http
   }
-  get (path, params, headers) {
-    return this._sendRequest('GET', path, params, headers)
+  get (path) {
+    return this._sendRequest('GET', path)
   }
-  post (path, params, headers) {
-    return this._sendRequest('POST', path, params, headers)
+  post (path, body) {
+    return this._sendRequest('POST', path, body)
   }
-  delete (path, params, headers) {
-    return this._sendRequest('DELETE', path, params, headers)
+  delete (path, body) {
+    return this._sendRequest('DELETE', path, body)
   }
-  _sendRequest (method, path, params, headers) {
-    return new Promise((resolve, reject) => {
-      this.params = JSON.stringify(params)
-      this.options = {
-        hostname: this.baseUrl,
-        path: path,
-        method: method,
-        headers: method === 'GET' ? null : this._setHeaders(headers, this.params)
-      }
-      this.req = http.request(this.options, res => {
-        res.setEncoding('utf8')
-        const body = []
-        res.on('data', (chunk) => body.push(chunk))
-        res.on('end', () => resolve(body.join('')))
-      })
-      this.req.on('error', err => reject(err))
-      this.req.write(this.params)
-      this.req.end()
-    })
+  patch (path, body) {
+    return this._sendRequest('PATCH', path, body)
   }
-  _setHeaders (headers) {
-    let defaultHeaders = {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(this.params)
+  _sendRequest (method, path, body) {
+    let options = {
+      uri: this.baseUrl + path,
+      method: method,
+      body: body,
+      json: true
     }
-    if (headers) {
-      for (var header in headers) {
-        defaultHeaders[header] = headers[header]
-      }
-    }
-    return defaultHeaders
+    return this.http(options)
   }
 }
 
