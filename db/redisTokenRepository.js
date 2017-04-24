6ac7cb7f8e30206575960d12aef8c3c33489d710
constantin guidon <constantin.guidon@gmail.com>
2017-04-24 18:00:10 +0200

    [feature] redis token repository inherit from token repository

--- db/redisTokenRepository.js
+++ db/redisTokenRepository.js
@@ -1,24 +1,15 @@
-const redis = require('../config/environment').redisClient
 const uuidV4 = require('uuid/v4')
+const TokenRepository = require('./tokenRepository')
 
-class RedisTokenRepository {
-  constructor (ttl = 86400) {
-    this.redis = redis
-    this.ttl = ttl
-  }
-  getToken (token) {
-    return this.redis.getAsync(token)
-  }
+class RedisTokenRepository extends TokenRepository {
   setToken (user) {
     const token = this._generateSessionId()
-    this.redis.set(token, user.id, 'EX', this.ttl)
+    super.setToken(token, user.id)
     return token
   }
+
   _generateSessionId () {
     return uuidV4()
-  }
-  deleteToken (token) {
-    return this.redis.delAsync(token)
   }
 }
 
---
+++ db/tokenRepository.js
@@ -0,0 +1,22 @@
+const redis = require('../config/environment').redisClient
+
+class TokenRepository {
+  constructor (ttl) {
+    this.redis = redis
+    this.ttl = ttl
+  }
+
+  getToken (token) {
+    return this.redis.getAsync(token)
+  }
+
+  setToken (token, value) {
+    this.redis.set(token, value, 'EX', this.ttl)
+  }
+
+  deleteToken (token) {
+    return this.redis.delAsync(token)
+  }
+}
+
+module.exports = TokenRepository
