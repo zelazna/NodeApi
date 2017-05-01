bed844a5fd8cc42e7943310d2193c0c63969b591
zelazna <constantin.guidon@gmail.com>
2017-05-01 12:39:20 +0200

    [fix] db url

--- config/environment.js
+++ config/environment.js
@@ -6,19 +6,19 @@ const environment = process.env.NODE_ENV || 'DEVELOPMENT'
 
 require(`./environments/${environment.toLowerCase()}`)
 
-const database = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-    port: process.env.DB_PORT
+const match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+const database = new Sequelize(match[5], match[1], match[2], {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  port: match[4],
+  host: match[3],
+  logging: false,
+  dialectOptions: {
+    ssl: true
   }
-)
+})
 
-const redisClient = redis.createClient({url: process.env.REDIS_URL}).on('error', err => console.log('Error ' + err))
+const redisClient = redis.createClient({ url: process.env.REDIS_URL }).on('error', err => console.log('Error ' + err))
 bluebird.promisifyAll(redis.RedisClient.prototype)
 bluebird.promisifyAll(redis.Multi.prototype)
 
