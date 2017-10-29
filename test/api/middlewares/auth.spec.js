5491b099ec3af2e5007e98a0f30c800d3c9751bf
Constantin Guidon <constantin.guidon@gmail.com>
2017-10-29 20:09:52 +0100

    [fix] middleware test

--- test/api/middlewares/auth.spec.js
+++ test/api/middlewares/auth.spec.js
@@ -13,8 +13,8 @@ describe('auth middleware', () => {
       expect(auth).to.be.a.Function
     })
 
-    it('should accept 4 arguments', () => {
-      expect(auth.length).to.equal(4)
+    it('should accept 3 arguments', () => {
+      expect(auth.length).to.equal(3)
     })
   })
 
@@ -24,12 +24,6 @@ describe('auth middleware', () => {
       const token = repository.setToken({ id: 1 })
       const req = { headers: { 'X-AUTH-TOKEN': token } }
       auth(req, {}, nextSpy)
-    })
-    it('should executed a function on unsuccessful auth', (done) => {
-      const next = () => {}
-      const req = { headers: { 'x-auth-token': '' } }
-      const errCallback = done
-      auth(req, {}, next, errCallback)
     })
   })
 })
